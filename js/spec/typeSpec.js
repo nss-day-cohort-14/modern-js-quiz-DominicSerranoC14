5867dd09fe26e4d9201308160a01c5fb8286866d
Dominic Serrano <dmsrojo@gmail.com>
2016-07-17 21:18:13 -0500

    Edited type.spec to accept a drone type. All testing works for base logics

--- js/spec/typeSpec.js
+++ js/spec/typeSpec.js
@@ -1,29 +1,29 @@
-// 'use strict';
-//
-// //Browserify
-// let Drone = require('../droneType');
-//
-// ////////////////////////
-// //Begin function testing
-// ////////////////////////
-//
-// //Test for a base type function, use this as template for each type of robot
-// describe('Test for base type function', function() {
-//
-//   it('Should describe a base type function', function () {
-// 		expect(Drone).toBeDefined();
-// 	});
-//
-//   it('Drone should have a health, attack, name property', function () {
-//     let little = new Drone();
-// 		expect(little.health).toBe(50);
-// 		expect(little.attack).toBe(10);
-// 		expect(little.name).toBe(null);
-// 	});
-//
-//   it('Drone should have a type property', function () {
-//     let little = new Drone();
-// 		expect(little.type).toBe('Drone');
-// 	});
-//
-// });//end describe base type function
+'use strict';
+
+//Browserify
+let Drone = require('../type');
+
+////////////////////////
+//Begin function testing
+////////////////////////
+
+//Test for a base type function, use this as template for each type of robot
+describe('Test for base type function', function() {
+
+  it('Should describe a base type function', function () {
+		expect(Drone).toBeDefined();
+	});
+
+  it('Drone should have a health, attack, name property', function () {
+    let little = new Drone.Drone();
+		expect(little.health).toBe(50);
+		expect(little.attack).toBe(10);
+		expect(little.name).toBe(null);
+	});
+
+  it('Drone should have a type property', function () {
+    let little = new Drone.Drone();
+		expect(little.type).toBe('Drone');
+	});
+
+});//end describe base type function
