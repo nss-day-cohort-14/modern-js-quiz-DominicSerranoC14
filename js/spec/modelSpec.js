0785e8f5a68b1b9737c9da351dae11ec82bca2ed
Dominic Serrano <dmsrojo@gmail.com>
2016-07-17 20:45:59 -0500

    Made changes to modelSpec.js to accurately test the attack and deffense of the attackDrone defaul, also condsidering each additional statGen based on model

--- js/spec/modelSpec.js
+++ js/spec/modelSpec.js
@@ -1,41 +1,43 @@
-// 'use strict';
-//
-// //Browserify
-// let DroneType = require('../droneModel');
-//
-// ////////////////////////
-// //Begin function testing
-// ////////////////////////
-//
-// //Test for a base type function, use this as template for each type of robot
-// describe('Should describe a base model function', function() {
-//
-//   it('Should describe the base function', function () {
-//     let droney = new DroneType();
-// 		expect(droney).toBeDefined();
-// 	});
-//
-//     //function should have a method that is its model name
-//     it('Should describe a model name', function () {
-//     let droney = new DroneType();
-//     droney.model = 'Attack Drone';
-// 		expect(droney.model).toBe('Attack Drone');
-// 	});
-//
-//   //function should have an attack > 10
-//   it('Attack should be > 10, <= 30', function () {
-//     let droney = new DroneType();
-//     droney.attack = droney.attack + Math.floor(Math.random() * (20 - 10) + 10);
-// 		expect(droney.attack).toBeGreaterThan(10);
-//     expect(droney.attack).toBeLessThan(30);
-// 	});
-//
-//   //function should have a min health > 50
-//   it('Defense should be > 50, <=60', function () {
-//     let droney = new DroneType();
-//     droney.health = droney.health + Math.floor(Math.random() * (20 - 1) + 1);
-//     expect(droney.health).toBeGreaterThan(50);
-//     expect(droney.health).toBeLessThan(70);
-//   });
-//
-// });//end test for model functions
+'use strict';
+
+//Browserify
+let DroneType = require('../droneModel');
+
+////////////////////////
+//Begin function testing
+////////////////////////
+
+//These tests will pass with each model type, the default model type is the droneAttack model, but all models are based on each other
+
+//Test for a base type function, use this as template for each type of robot
+describe('Should describe a base model function', function() {
+
+  it('Should describe the base function', function () {
+    let droney = new DroneType.droneAttack();
+		expect(droney).toBeDefined();
+	});
+
+    //function should have a method that is its model name
+    it('Should describe a model name', function () {
+    let droney = new DroneType.droneAttack();
+    droney.model = 'Attack Drone';
+		expect(droney.model).toBe('Attack Drone');
+	});
+
+  //function should have an attack > 10
+  it('Attack should be > 10, <= 30', function () {
+    let droney = new DroneType.droneAttack();
+    droney.attack = droney.attack + Math.floor(Math.random() * (20 - 10) + 10);
+		expect(droney.attack).toBeGreaterThan(10);
+    expect(droney.attack).toBeLessThan(55);
+	});
+
+  //function should have a min health > 50
+  it('Defense should be > 50, <=60', function () {
+    let droney = new DroneType.droneAttack();
+    droney.health = droney.health + Math.floor(Math.random() * (20 - 1) + 1);
+    expect(droney.health).toBeGreaterThan(50);
+    expect(droney.health).toBeLessThan(90);
+  });
+
+});//end test for model functions
